b72a7369774a3293d22c1dced0bb7fc419b9156d
Muhammad Salman Aslam <salmanldhr@gmail.com>
2024-10-28 10:39:07 +0000

    learn variables

--- learnjs_1/01.js
+++ learnjs_1/01.js
@@ -1,1 +1,19 @@
-console.log("Salman!");
+const acountId = 12345
+
+var acountEmail = "ldhr123@google.com"
+
+let acountPassword = 21212121
+
+acountCity = "lodhran"
+
+console.log(acountId);
+
+acountEmail = "only5555@google.com"
+
+acountPassword = 111111111
+
+acountCity = "lahore"
+
+let acountState;
+
+console.table([acountEmail, acountPassword, acountCity, acountState]);
