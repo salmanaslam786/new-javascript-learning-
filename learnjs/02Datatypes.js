c63ef3170efc89bbc5c266bb90424f4635abe6a0
Muhammad Salman Aslam <salmanldhr@gmail.com>
2024-10-28 11:07:39 +0000

    changing name and learn data types

---
+++ learnjs/02Datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert () // we are using nodejs, not browser 
+// always write code in seprate line
+
+// code readability should be high!
+
+// Different data types 
+// number => 2 to power 53 
+// if number is large then used bigint
+// string => "" write in double or single quotes
+// boolean => true/false 
+// null => standalone value 
+// undefined => 
+// symbole => unique 
+
+
+// object learn in detail 
+
+// when you want to know the type of string then 
+
+console.log(typeof "salman");
+
+console.log(typeof undefined); // output = undefined 
+console.log(typeof null);  // object 
+
