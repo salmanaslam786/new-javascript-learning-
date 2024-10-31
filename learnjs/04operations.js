2c5b21c0fcb678eb3f7edb2f43041ac481b0d007
Muhammad Salman Aslam <salmanldhr@gmail.com>
2024-10-31 15:35:07 +0000

    operations

---
+++ learnjs/04operations.js
@@ -0,0 +1,23 @@
+let value = 4 
+
+let negvalue = -value 
+
+//console.log(negvalue);
+
+//console.log(2**3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 2);
+//console.log(1+2 + "2")
+
+console.log(3 + 4 * 5 % 3);
+
+
+let gameCounter = 100 
+gameCounter++;
+
+console.log(gameCounter);
+
+//dont use complicated code like above instead of write simple code 
+// asignment read about prefix and postfix in mdn documents 
