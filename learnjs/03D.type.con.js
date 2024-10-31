466121e025addd790646f7993a23b8df12dea5b9
Muhammad Salman Aslam <salmanldhr@gmail.com>
2024-10-31 14:51:03 +0000

    datatypeconversion

---
+++ learnjs/03D.type.con.js
@@ -0,0 +1,29 @@
+let score = true
+
+//console.log(typeof score); 
+
+let valueInNumber = Number(score);
+
+//console.log(typeof valueInNumber); 
+
+//console.log(valueInNumber);
+
+// "33" => 33 string convert to number
+// "33abc" => NaN
+// true => 1; false => 0 
+
+let isLoggedIn = 1 
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false 
+// "" => false  empty string 
+// "salman" => true 
+//we can change things in numbers and boolean 
+
+let someNumber = 33 
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
