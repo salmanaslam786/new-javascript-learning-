8a487f4b9b8a9e1b923551d4e6c6a2ab220c0082
Muhammad Salman Aslam <salmanldhr@gmail.com>
2024-11-04 17:52:32 +0000

    strings

---
+++ learnjs/06string.js
@@ -0,0 +1,10 @@
+const name = "salman"
+const repoCount = 05 
+
+// how to concatinate these 
+//console.log(name + repoCount + " value");
+
+console.log(`Hello m name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("SalmanPc");
+
