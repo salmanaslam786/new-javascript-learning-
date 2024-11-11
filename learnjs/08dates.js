5e91336a18599e0e4a28d9e1a1dd485c594693c5
Muhammad Salman Aslam <salmanldhr@gmail.com>
2024-11-11 15:43:23 +0000

    dates more

--- learnjs/08dates.js
+++ learnjs/08dates.js
@@ -2,6 +2,36 @@
 const log = console.log;
 let myDate = new Date();
 
-console.log(myDate.toString());
+/*console.log(myDate.toString());
 console.log(myDate.toDateString());
 log(myDate.toLocaleString());
+log(myDate.toISOString());
+log(typeof myDate);*/
+
+//let myCreatedDate = new Date(2024, 10, 11);
+//let myCreatedDate = new Date(2024, 10, 11, 5, 3);
+let myCreatedDate = new Date("2024-10-11");
+//log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+//log(myTimeStamp);
+
+//log(myCreatedDate.getTime());
+
+//log(Math.floor(Date.now()/1000));
+
+let newDate = new Date();
+log(newDate.getMonth() + 1);
+
+
+//let dateTime = newDate.toLocaleString("default", {
+    weekday: "long", 
+    
+})
+//log(dateTime.toLocaleString());
+
+newDate.toLocaleString("default", {
+    weekday: "long", 
+    
+})
+log(newDate.toLocaleString());
